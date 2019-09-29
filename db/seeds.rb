4b0db3d3ca28e686903f6dedf0eb33f80ab8b528
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-09-29 18:18:58 +0900

    membersテーブルに10人分のデータを投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds/developmentディレクトリのしたに「テーブル名.rb」があれば、
+# それをrequireメソッドで実行する
+# 本番モードでは、db/seeds/productionディレクトリの下のファイルを実行する
+
+table_names = %w(members)
+table_names.each do |table_name|
+  path = Rails.root.join('db/seeds', Rails.env, table_name + '.rb')
+  if File.exist?(path)
+    puts "Creating #{table_name}"
+    require path
+  end
+end
